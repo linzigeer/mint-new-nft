88cc65e2de10f1e2a48b3e8a2fcb28ca82ce19d4
linzigeer <263720492@qq.com>
2024-11-17 22:46:28 +0800

    add nft-nft.json and create-nft.ts

--- create-nft.ts
+++ create-nft.ts
@@ -3,7 +3,7 @@ import { getExplorerLink, getKeypairFromFile, airdropIfRequired } from "@solana-
 
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
-import { keypairIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi";
+import { keypairIdentity, generateSigner, percentAmount, publicKey } from "@metaplex-foundation/umi";
 
 const connection = new Connection(clusterApiUrl("devnet"));
 const user = await getKeypairFromFile();
@@ -21,16 +21,27 @@ const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
 umi.use(keypairIdentity(umiUser));
 console.log("set up umi instance for user");
 
-const collectionMint = generateSigner(umi);
-const transaction = await createNft(umi, {
-    name: "My Collection",
-    symbol: "ZLL",
-    uri: "https://raw.githubusercontent.com/linzigeer/mint-new-nft/refs/heads/main/nft.json",
-    sellerFeeBasisPoints: percentAmount(0),
-    mint: collectionMint,
-})
+const collectionAddress = publicKey("6DpWZ8eknB5Y17ciLGFsfBmdzmhGv4Hu2yEV7L5WrsrN");
+
+console.log("Creating NFT...");
+
+const mint = generateSigner(umi);
+const transaction = await createNft(
+    umi,
+    {
+        name: "My NFT",
+        uri: "https://...",
+        sellerFeeBasisPoints: percentAmount(0),
+        mint,
+        collection:
+        {
+            key: collectionAddress,
+            verified: false,
+        }
+    }
+)
 
 await transaction.sendAndConfirm(umi);
-const createdCollectionNft = await fetchDigitalAsset(umi, collectionMint.publicKey);
+const createdNFT = await fetchDigitalAsset(umi, mint.publicKey);
 
-console.log(`Created Collection! Address is ${getExplorerLink("address", createdCollectionNft.mint.publicKey, "devnet")}`);
+console.log(`Created NFT! Address is ${getExplorerLink("address", createdNFT.mint.publicKey, "devnet")}`);
